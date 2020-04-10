e8a2f38bf3dc746993ac7118d376bc2e9110ab3f
Stephanierlandry <stephanierlandry@gmail.com>
2020-04-10 11:10:52 -0500

    React Router installed. Location Button added.

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 import Foundation from 'react-foundation';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>NPR Locator</h1>
+      <button className="button">Find By Location</button>
   </div>
 );
