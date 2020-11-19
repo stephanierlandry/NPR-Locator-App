606e8036c614c839fb076c9273ed1339f8e86d04
Stephanierlandry <stephanierlandry@gmail.com>
2020-11-18 19:34:12 -0600

    functions getting stations moved to appropriate file

---
+++ app/imports/helpers/locationHelpers.jsx
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
+import { EJSON } from 'meteor/ejson';
+
+const onSuccess = (position) => {
+
+  Session.set('lat', position.coords.latitude);
+  Session.set('lon', position.coords.longitude);
+
+  Meteor.call('getLocationByZip',position.coords.latitude, position.coords.longitude, (e, r) => {
+    const parsedZipResult = EJSON.parse(r.content);
+    Session.set('zip', parsedZipResult.postalCodes[0].postalCode);
+  })
+
+  const zipCodeResult = Session.get('zip');
+
+  Meteor.call('getNprData', zipCodeResult, (e,r) => {
+    const parsedNprResult = EJSON.parse(r.content);
+    const nprStationResult = parsedNprResult.items
+    const stations = [];
+
+    for(let ii of nprStationResult){
+      const stationData = ii.attributes.brand;
+        stations.push({
+          'call' : `${stationData.call}`,
+          'frequency' : `${stationData.frequency}`,
+          'city' : `${stationData.marketCity}`,
+          'state' : `${stationData.marketState}`,
+          'name' : `${stationData.name}`,
+          'tagline' : `${stationData.tagline}`})
+    }
+    console.log('im still working', stations)
+
+    Session.set('nprStations', stations);
+  })
+}
+
+
+const onError = (error) => {
+  alert(error.message)
+  Session.set('geoLocateError', error.message);
+}
+
+export { onError, onSuccess };
